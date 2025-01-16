e50b1525f4554be132fe0583cc3641a1fd95134b
VINAYAK TREPANKAR <53932355+vinayakt62@users.noreply.github.com>
2025-01-16 14:20:54 +0530

    Add files via upload

---
+++ Send Email/frmSendEmail.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TechASP.SmartAdvocateDiary.SMS_Sending
+{
+    partial class frmSendEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSendEmail));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lnkAttachFile = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMailFrom = new System.Windows.Forms.TextBox();
+            this.lblAttachment = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(336, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 134);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 19);
+            this.label1.TabIndex = 1547;
+            this.label1.Text = "** YOUR ACCOUNT MUST BE ON GMAIL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(48, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "} S E N D - E M A I L {";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(125, 224);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(310, 27);
+            this.txtTo.TabIndex = 3;
+            this.txtTo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtTo_KeyDown);
+            this.txtTo.Validating += new System.ComponentModel.CancelEventHandler(this.txtTo_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TO :";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(125, 268);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(310, 27);
+            this.txtSubject.TabIndex = 4;
+            this.txtSubject.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSubject_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SUBJECT :";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(125, 308);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(310, 89);
+            this.txtBody.TabIndex = 5;
+            this.txtBody.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBody_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "MESSAGE :";
+            // 
+            // lnkAttachFile
+            // 
+            this.lnkAttachFile.AutoSize = true;
+            this.lnkAttachFile.Location = new System.Drawing.Point(121, 408);
+            this.lnkAttachFile.Name = "lnkAttachFile";
+            this.lnkAttachFile.Size = new System.Drawing.Size(106, 19);
+            this.lnkAttachFile.TabIndex = 6;
+            this.lnkAttachFile.TabStop = true;
+            this.lnkAttachFile.Text = "ATTACHMENTS";
+            this.lnkAttachFile.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkAttachFile_LinkClicked);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(259, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "C A N C E L";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(145, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "S E N D";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "PASSWORD :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 178);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(310, 27);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassword_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 138);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "YOUR EMAIL :";
+            // 
+            // txtMailFrom
+            // 
+            this.txtMailFrom.Location = new System.Drawing.Point(125, 135);
+            this.txtMailFrom.Name = "txtMailFrom";
+            this.txtMailFrom.Size = new System.Drawing.Size(310, 27);
+            this.txtMailFrom.TabIndex = 1;
+            this.txtMailFrom.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMailFrom_KeyDown);
+            this.txtMailFrom.Validating += new System.ComponentModel.CancelEventHandler(this.txtMailFrom_Validating);
+            // 
+            // lblAttachment
+            // 
+            this.lblAttachment.AutoSize = true;
+            this.lblAttachment.Location = new System.Drawing.Point(24, 424);
+            this.lblAttachment.Name = "lblAttachment";
+            this.lblAttachment.Size = new System.Drawing.Size(49, 19);
+            this.lblAttachment.TabIndex = 16;
+            this.lblAttachment.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmSendEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(473, 487);
+            this.Controls.Add(this.lblAttachment);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMailFrom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lnkAttachFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSendEmail";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SEND EMAIL";
+            this.Load += new System.EventHandler(this.frmSendEmail_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSendEmail_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel lnkAttachFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMailFrom;
+        private System.Windows.Forms.Label lblAttachment;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Send Email/frmSendEmail.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace TechASP.SmartAdvocateDiary.SMS_Sending
+{
+
+    public partial class frmSendEmail : Form
+    {
+        MailMessage message;
+        SmtpClient smtp;
+        public frmSendEmail()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSendEmail_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmSendEmail_Load(object sender, EventArgs e)
+        {
+            txtMailFrom.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtTo.Text = string.Empty;
+            txtSubject.Text = string.Empty;
+            txtBody.Text = string.Empty;
+            lblAttachment.Text = string.Empty;
+            txtMailFrom.Focus();
+        }
+
+        private void lnkAttachFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                lblAttachment.Text = openFileDialog1.FileName;
+                lblAttachment.Visible = true;
+                lnkAttachFile.Visible = false;
+            }
+        }
+        void smtp_SendCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Email sending cancelled!", "M E S S A G E");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else
+            {
+                MessageBox.Show("Email sent sucessfully!", "M E S S A G E");
+            }
+
+            button2.Visible = false;
+            button1.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                smtp.SendAsyncCancel();
+                MessageBox.Show("Email sending cancelled!", "M E S S A G E");
+            }
+            catch (Exception ex) { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Enabled = false;
+
+                message = new MailMessage();
+
+                if (txtMailFrom.Text != string.Empty && txtPassword.Text != string.Empty && txtTo.Text != string.Empty)
+                {
+                    message.To.Add(txtTo.Text);
+                    message.Subject = txtSubject.Text;
+                    message.From = new MailAddress(txtMailFrom.Text);
+                    message.Body = txtBody.Text;
+                    if (lblAttachment.Text.Length > 0)
+                    {
+                        if (System.IO.File.Exists(lblAttachment.Text))
+                        {
+                            message.Attachments.Add(new Attachment(lblAttachment.Text));
+                        }
+                    }
+
+                    // set smtp details
+                    smtp = new SmtpClient("patilswapnil1889@gmail.com");
+                    smtp.Port = 25;
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new NetworkCredential(txtMailFrom.Text, txtPassword.Text);
+                    smtp.SendAsync(message, message.Subject);
+                    smtp.SendCompleted += new SendCompletedEventHandler(smtp_SendCompleted);
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Please provide valid email address", "E R R O R", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    txtMailFrom.SelectAll();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                button2.Visible = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void txtMailFrom_Validating(object sender, CancelEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex rEmail = new System.Text.RegularExpressions.Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+            if (txtMailFrom.Text.Length > 0)
+            {
+                if (!rEmail.IsMatch(txtMailFrom.Text))
+                {
+                    MessageBox.Show("Please provide valid email address", "E R R O R", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtMailFrom.Focus();
+                }
+            }
+        }
+
+        private void txtMailFrom_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtMailFrom.Text != string.Empty)
+                {
+                    txtPassword.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("ENTER YOUR EMAIL ID!", "M E S S A G E");
+                    txtMailFrom.Focus();
+                }
+            }
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtPassword.Text != string.Empty)
+                {
+                    txtTo.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("ENTER YOUR EMAIL ID PASSWORD!", "M E S S A G E");
+                    txtMailFrom.Focus();
+                }
+            }
+        }
+
+        private void txtTo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtTo.Text != string.Empty)
+                {
+                    txtSubject.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("ENTER SENDER EMAIL ID!", "M E S S A G E");
+                    txtTo.Focus();
+                }
+            }
+        }
+
+        private void txtSubject_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtBody.Focus();
+            }
+        }
+
+        private void txtBody_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.Focus();
+            }
+        }
+
+        private void txtTo_Validating(object sender, CancelEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex rEmail = new System.Text.RegularExpressions.Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            //System.Text.RegularExpressions.Regex rEmail = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9] [\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$");
+
+            if (txtTo.Text.Length > 0)
+            {
+                if (!rEmail.IsMatch(txtTo.Text))
+                {
+                    MessageBox.Show("Please provide valid email address", "E R R O R", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtTo.Focus();
+                }
+            }
+        }
+    }
+}
